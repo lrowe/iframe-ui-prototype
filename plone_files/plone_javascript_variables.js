763a57ffe9c12612d5d498fd1b8c503e363499d1
Laurence Rowe <laurence@lrowe.co.uk>
2011-06-06 01:15:35 +0100

    Export of Plone front page from Plone 4.1 as admin and as anon. (Export makes it HTML not XHTML.)

---
+++ plone_files/plone_javascript_variables.js
@@ -0,0 +1,9 @@
+
+/* - plone_javascript_variables.js - */
+var portal_url = 'http://localhost:8080/Plone';
+var form_modified_message = 'Your form has not been saved. All changes you have made will be lost.';
+var form_resubmit_message = 'You already clicked the submit button. Do you really want to submit this form again?';
+var external_links_open_new_window = 'false';
+var mark_special_links = 'false';
+var ajax_noresponse_message = 'No response from server. Please try again later.';
+
