85f5116914b7c5d5de08bcae464a8c9697b24e4d
Laurence Rowe <laurence@lrowe.co.uk>
2011-06-10 12:23:07 +0100

    Add js standards to dropdown.js for easier checking

--- dropdown.js
+++ dropdown.js
@@ -1,3 +1,5 @@
+/*jslint white:false, onevar:true, undef:true, nomen:true, eqeqeq:true, plusplus:true, bitwise:true, regexp:true, newcap:true, immed:true, strict:false, browser:true */
+/*global jQuery:false, document:false, window:false, location:false */
 
 /* - dropdown.js - */
 /*
